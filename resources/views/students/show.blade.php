9867acfdb08c81620cf6384e4fd9e8ef94c5fb9e
Muhammad Alvian <muhammadalfian901785@gmail.com>
2022-01-20 15:29:13 +0700

    commit

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ $student->nim }}</div>
+                    <div class="card-body">
+                        <img width="150px" src="{{ asset('storage/'.$student->photo) }}" ><br>
+                        Name : {{ $student->name }} <br>
+                        Class : {{ $student->kelas->class_name }} <br>
+                        Department : {{ $student->department }} <br>
+                        Phone Number : {{ $student->phone_number }} <br>
+                        Created at : {{ $student->created_at }} <br>
+                        Updated at : {{ $student->updated_at }} <br>
+                    </div>                  
+                    </div>                                      
+                </div>               
+            </div>            
+        </div>       
+    </div>
+@endsection
